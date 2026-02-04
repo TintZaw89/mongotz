ad642c9e5f95c5baae0cc30761665dacfe6d4299
TintZaw89 <thintzaw89@gmail.com>
2026-02-04 10:22:12 +0630

    Remove unnecessary Content

--- src/main/java/com/mongodbtz/mongotz/IMDbMovieRepositoryImpl.java
+++ src/main/java/com/mongodbtz/mongotz/IMDbMovieRepositoryImpl.java
@@ -1,13 +1,9 @@
 package com.mongodbtz.mongotz;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.mongodb.core.MongoTemplate;
-
 import java.util.List;
 
 public class IMDbMovieRepositoryImpl {
-    //@Autowired
     private IMDbMovieRepository imDbMovieRepository;
 
     public List<IMDbMovie> SortedByFieldAsc() {
